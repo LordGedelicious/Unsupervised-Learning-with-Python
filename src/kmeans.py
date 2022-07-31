74546c0121ce1cb16c8cd24ae34bc24f013cc8db
Gede Prasidha <gede.prasidha@gmail.com>
2022-07-31 23:05:55 +0700

    Create kmeans.py

---
+++ src/kmeans.py
@@ -0,0 +1,11 @@
+# The end goal of a K-means algorithm is to find clusters in a dataset by decreasing the sum of squared errors from a number of k centroids.
+# The following is the steps of the K-means algorithm:
+# 1. Set the centroids to be random points in the dataset as much as k
+# 2. Calculate the sum of squared deviations (errors) from the centroids
+# 3. Assign a centroid to each point in the dataset
+# 4. Calculate the new sum of total errors and compare it to the previous sum of total errors
+# 5. If it's lesser, switch and recalculate the centroids.
+# 6. Repeat :D
+
+def setCentroids(data, k):
+    pass
